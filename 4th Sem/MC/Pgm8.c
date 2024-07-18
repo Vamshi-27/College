c4214e7b7bff60efbc3ad2bbfea513c0a4a087b7
Vamshi-27 <vamshira18@outlook.com>
2024-07-18 17:34:12 +0530

    sync

---
+++ 4th Sem/MC/Pgm8.c
@@ -0,0 +1,28 @@
+// Bubble sort program
+
+#include <stdio.h>
+#include<lpc214x.h>
+
+int main()
+{
+    unsigned int i,j;
+    unsigned char a[]={5,1,4,2,3;
+    unsigned char temp;
+    do
+    {
+        for(i=0;i<5;i++)
+        {
+            for(j=0;j<5-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+    }
+    while(1);
+    return 0;
+}
