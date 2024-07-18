91c9b6adae41605ed90db6cc694741f9351cb5c0
Vamshi-27 <vamshira18@outlook.com>
2024-07-18 17:38:04 +0530

    sync

--- 4th Sem/MC/Pgm8.c
+++ 4th Sem/MC/Pgm8.c
@@ -6,23 +6,19 @@
 int main()
 {
     unsigned int i,j;
-    unsigned char a[]={5,1,4,2,3;
+    unsigned char a[]={5,1,4,2,3};
     unsigned char temp;
-    do
+    for(i=0;i<5;i++)
     {
-        for(i=0;i<5;i++)
+        for(j=0;j<5-i-1;j++)
         {
-            for(j=0;j<5-i-1;j++)
+            if(a[j]>a[j+1])
             {
-                if(a[j]>a[j+1])
-                {
-                    temp=a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
-                }
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
             }
         }
     }
-    while(1);
     return 0;
 }
