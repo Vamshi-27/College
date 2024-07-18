ffdea070475a75544e5145d2fd1b042930013f8a
Vamshi-27 <vamshira18@outlook.com>
2024-07-18 15:59:41 +0530

    sync

---
+++ 4th Sem/MC/Program7.C
@@ -0,0 +1,17 @@
+// Factorial of a number
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<lpc214x.h>
+
+int main()
+{
+    unsigned int i;
+    unsigned char num=5;
+    unsigned long fact=1;
+    for(i=1;i<=num;i++)
+    {
+        fact=fact*i;
+    }
+    return 0;
+}
