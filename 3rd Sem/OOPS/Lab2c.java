1bb9e0f7c50d1ccd76c406a038c2949ee68c1dcc
Vamshi-27 <vamshira18@outlook.com>
2024-07-20 10:09:49 +0530

    sync

---
+++ 3rd Sem/OOPS/Lab2c.java
@@ -0,0 +1,16 @@
+// Program to initialize 10 array elements and find the sum of first 4 elements using enhanced for loop.
+class Lab2c {
+    public static void main(String[] args) 
+    {
+        int sum = 0, count = 0;
+        int a[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        for (int x : a) 
+        {
+            sum = sum + x;
+            count++;
+            if (count == 4)
+                break;
+        }
+        System.out.println("Sum= " + sum);
+    }
+}
