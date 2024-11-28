e687d8361a69804caa44644f62e77dc660d14365
Vamshi-27 <vamshira18@outlook.com>
2024-11-28 09:32:50 +0530

    commit

---
+++ 5th Sem/CN/Client.java
@@ -0,0 +1,32 @@
+import java.net.*; 
+import java.io.*; 
+public class Client
+{
+	public static void main( String args[ ] ) throws Exception
+	{
+		Socket sock = new Socket( "127.0.0.1", 4000);
+		System.out.print("Enter the file name to display the contents of file");
+		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in)); 
+		String fname = keyRead.readLine();
+		try
+		{
+			OutputStream ostream = sock.getOutputStream( ); 
+			PrintWriter pwrite = new PrintWriter(ostream, true); 
+			pwrite.println(fname); 
+			InputStream istream = sock.getInputStream();
+			BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream)); 
+			String str;
+			while((str = socketRead.readLine()) != null)
+			{
+				System.out.println(str);
+			}
+			pwrite.close();
+			socketRead.close();
+			keyRead.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("An error occured while opening the file");
+		}
+	}
+}
