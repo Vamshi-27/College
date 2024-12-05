31c548aa88430e11a35c25a3822881634a93541d
Vamshi-27 <vamshira18@outlook.com>
2024-12-05 10:14:13 +0530

    commit

--- 5th Sem/CN/Server.java
+++ 5th Sem/CN/Server.java
@@ -1,3 +1,7 @@
+// // Lab 7: Using TCP/IP sockets, write a client – server program to make the client send the file name and to make the server send back the contents of the requested file if present.
+
+// Server.java
+
 import java.net.*;
 import java.io.*;
 
