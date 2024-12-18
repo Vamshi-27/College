23ffdef22501b62d1902f4edc830eda400150782
Vamshi-27 <vamshira18@outlook.com>
2024-12-18 14:53:31 +0530

    commit

--- 5th Sem/CG/Mouse2.c
+++ 5th Sem/CG/Mouse2.c
@@ -1,74 +1,48 @@
-#include <GL/glut.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<GL/glut.h>
+
+void myinit();
+void display();
+void myMouse(int button,int state,int x,int y);
+
+void main(int argc,char **argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutCreateWindow("Simple OpenGL Program");
+	glutInitWindowPosition(250,250);
+	glutInitWindowSize(500,500);
+	glutDisplayFunc(display);
+	myinit();
+	glutMouseFunc(myMouse);
+	glutMainLoop();
+}
 
 void myinit()
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    gluOrtho2D(0.0, 100.0, 0.0, 100.0);
-    glPointSize(6.0);
+	glClearColor(0.0,0.0,0.0,0.0);
+	gluOrtho2D(0.0,100.0,0.0,100.0);
 }
 
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_LINES);
-
-    glVertex2i(10, 10);
-    glVertex2i(10, 90);
-    glEnd();
-    glFlush();
-}
-void display1()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(30, 80);
-    glVertex2i(70, 80);
-    glVertex2i(70, 20);
-    glVertex2i(30, 20);
-
-    glEnd();
-    glBegin(GL_POINTS);
-
-    glVertex2i(60, 60);
-    glEnd();
-    glFlush();
-}
-void demo_menu(int id)
-{
-    switch (id)
-    {
-    case 1:
-        glutStrokeCharacter(GLUT_STROKE_MONO_ROMAN, '@');
-        break;
-    case 2:
-        glRasterPos2f(10.0, 50.0);
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'c');
-        break;
-    case 3:
-        display1();
-        break;
-    case 4:
-        exit(0);
-        break;
-    }
-    glFlush();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINES);
+        glColor3f(1.0,0.0,0.0);
+        glVertex2i(10,60);
+        glColor3f(0.0,1.0,0.0);
+        glVertex2i(15,75);
+	glEnd();
+	glFlush();
 }
 
-void main(int argc, char **argv)
+void myMouse(int button,int state,int x,int y)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutCreateWindow("simple openGL pgm");
-    glutInitWindowPosition(50, 100);
-    glutInitWindowSize(500, 500);
-    myinit();
-    glutCreateMenu(demo_menu);
-    glutAddMenuEntry("glut storke", 1);
-    glutAddMenuEntry("bitmap", 2);
-    glutAddMenuEntry("display", 3);
-    glutAddMenuEntry("exit", 4);
-    glutDisplayFunc(display);
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
-    glutMainLoop();
+	if(button==GLUT_RIGHT_BUTTON && state==GLUT_DOWN)
+	{
+		glutBitmapCharacter(GLUT_BITMAP_8_BY_13,'V');
+	}
+	else if(button==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+		exit(0);
+	glFlush();
 }
