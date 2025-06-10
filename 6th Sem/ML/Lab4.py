e42cf8921a6beb9f36efcd4132aea5730814df32
Vamshi-27 <vamshira18@outlook.com>
2025-06-10 21:07:16 +0530

    commit

---
+++ 6th Sem/ML/Lab4.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+d = pd.read_csv(r"C:\Users\Vamshi R A\OneDrive\Desktop\Datasets\Data.csv")
+a = d.iloc[:, [0, 1, 2, 3, 4, 5]].values
+t = d.iloc[:, 6].values
+
+for i in range(len(t)):
+    if t[i] == "Yes":
+        h = a[i].copy()
+        break
+
+for i in range(len(a)):
+    if t[i] == "Yes":
+        for j in range(len(h)):
+            if h[j] != a[i][j]:
+                h[j] = "?"
+
+print("Final hypothesis:", h)
