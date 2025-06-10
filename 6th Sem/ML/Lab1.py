0c141f7af2179cfcc63b15426195e6a95a24eead
Vamshi-27 <vamshira18@outlook.com>
2025-06-10 20:49:34 +0530

    commit

---
+++ 6th Sem/ML/Lab1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import seaborn as s
+import numpy as np
+import matplotlib.pyplot as plt
+
+d=pd.read_csv(r"C:\Users\Vamshi R A\OneDrive\Desktop\Datasets\housingG.csv")
+
+nf=d.select_dtypes(include=['number']).columns
+
+for i in nf:
+    plt.figure(figsize=(6,3))
+    s.histplot(d[i],bins=30)
+    plt.show()
+
+for i in nf:
+    plt.figure(figsize=(6,3))
+    s.histplot(d[i])
+    plt.show()
+
+print("Outliers:")
+os={}
+for j in nf:
+    q1=d[j].quantile(0.25)
+    q3=d[j].quantile(0.75)
+    iqr=q3-q1
+    lb=q1-1.5*iqr
+    hb=q3+1.5*iqr
+    o=d[(d[j]<lb) | (d[j]>hb)]
+    os[j]=len(o)
+    print(f"{j}:{len(o)}")
